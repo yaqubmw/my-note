f1680bdb7779169a715f5290a1e176235969ebfc
Yaqub Muflih Wiraatmaja <yaqubmw2@gmail.com>
2023-08-31 21:37:03 +0700

    initial commit

---
+++ src/main/java/com/yaqubmw/mynote/MyNoteApplication.java
@@ -0,0 +1,13 @@
+package com.yaqubmw.mynote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyNoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yaqubmw/mynote/MyNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaqubmw.mynote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
